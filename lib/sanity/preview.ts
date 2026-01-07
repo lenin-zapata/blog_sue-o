b3322e823a2e30ed5bcc3612e9116cecf9d28860
Lenin Zapata <lenin.zapata.1993@gmail.com>
2026-01-07 12:56:01 -0500

    fix: update preview client to use hardcoded projectId and dataset

--- lib/sanity/preview.ts
+++ lib/sanity/preview.ts
@@ -1,8 +1,10 @@
 import { createClient } from 'next-sanity';
 
 export const previewClient = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  // projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  // dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId: 'sdejpl9x',
+  dataset: 'production',
   apiVersion: '2025-01-01',
   token: process.env.SANITY_API_TOKEN, // secreto: debe estar en .env.local y como Secret en Cloudflare
   useCdn: false, // imprescindible para ver borradores
